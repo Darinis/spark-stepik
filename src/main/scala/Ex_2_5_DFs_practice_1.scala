18f8dfaeed6d09f087e2199c8c9392f369c177a1
Darinis <darinisvm@gmail.com>
2022-12-10 13:37:56 +0300

    spark-stepik: Ex_2_5_DFs_practice_1

---
+++ src/main/scala/Ex_2_5_DFs_practice_1.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+//case object spark {
+//  lazy val ss: SparkSession = SparkSession.builder()
+//    .appName("2.2")
+//    .master("local")
+//    .getOrCreate
+//
+//  lazy val sc: SparkContext = ss.sparkContext
+//}
+
+object Ex_2_5_DFs_practice_1 extends App {
+  import org.apache.spark.sql.functions._
+  import org.apache.spark.sql.SaveMode
+
+  val customersDF = spark.ss.read
+    .option("inferSchema","true")
+    .option("header","true")
+    .csv("src/main/resources/mall_customers.csv")
+    .withColumn("Age",col("Age") + 2)
+
+  customersDF.printSchema()
+
+  val ageCondition = col("Age") between(30,35)
+  val genderCode = col("Gender") === "Male"
+
+  val incomeDF = customersDF
+    .filter(ageCondition)
+    .groupBy("Gender","Age")
+    .agg(
+      round(avg("Annual Income (k$)"),1).as("avg_income"))
+    .orderBy("Gender","Age")
+    .withColumn("gender_code",
+      when(genderCode,1).otherwise(0)
+    )
+
+  incomeDF.printSchema()
+//  incomeDF.show(false)
+
+  incomeDF.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/customers/file.parquet")
+}
